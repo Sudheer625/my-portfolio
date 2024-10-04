9fa2fbd9222766d2e659cf10cd625749ac0c14e7
Singidi Sai Naga Sudheer <123714207+Sudheer625@users.noreply.github.com>
2024-10-04 07:22:12 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import Hero from "./Components/Hero/Hero";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App(){
+  return(
+    <>
+    
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Contact/>
+    <Footer/>
+    </>
+  );
+}
+export default App
