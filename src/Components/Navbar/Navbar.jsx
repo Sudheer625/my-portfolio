38ff6bada7a2a55cad71656c56ba6d405d76e376
Singidi Sai Naga Sudheer <123714207+Sudheer625@users.noreply.github.com>
2024-10-04 07:20:19 +0530

    Add files via upload

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Navbar.css';
+function Navbar(){
+  return(
+    <>
+      <div className="navbar">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="about">About</a></li>
+          <li><a href="work">Work</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+      </div>
+    </>
+  );
+}
+export default Navbar
